7005fddfc311ef05786f53b8215aae0154844b57
Melissa LeBlanc-Williams <melissa@adafruit.com>
2022-09-27 08:55:35 -0700

    Snowpack config improvements for future fontawesome updates

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -14,16 +14,19 @@ export default {
   },
   plugins: [
     [
-      '@snowpack/plugin-run-script',
-        {
-          cmd: 'sass assets/sass/fontawesome.scss assets/css/fontawesome.css --style compressed -I node_modules/@fortawesome/fontawesome-free',
-          watch: '$1 --watch',
-          output: 'stream',
-        },
+      '@snowpack/plugin-run-script', {
+        // Copy latest webfonts to css folder
+        cmd: "cp -rf node_modules/@fortawesome/fontawesome-free/webfonts assets/css"
+      }
     ],
     [
-      '@snowpack/plugin-run-script',
-      {
+      '@snowpack/plugin-run-script', {
+        // Compile Font Awesome using the correct node package as a location
+        cmd: 'sass assets/sass/fontawesome.scss assets/css/fontawesome.css --style compressed -I node_modules/@fortawesome/fontawesome-free',
+      },
+    ],
+    [
+      '@snowpack/plugin-run-script', {
         cmd: 'sass assets/sass/main.scss assets/css/style.css --style compressed',
         watch: '$1 --watch',
         output: 'stream',
